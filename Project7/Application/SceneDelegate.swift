2f60cb99e499561cb9c677a0837bc6fb2476d883
Salakhoff <salakhoff.ilfat@gmail.com>
2023-02-02 13:12:10 +0300

    preparation for work

---
+++ Project7/Application/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  Project7
+//
+//  Created by Ильфат Салахов on 02.02.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowsScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowsScene)
+        let vc = ViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+    }
+}
+
